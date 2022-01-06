7e1188165e4e1b8e124df1ccffe7ec44489be5af
Noah2211 <noah.decroix3@gmail.com>
2022-01-06 16:33:04 +0100

    exerice done

--- index.php
+++ index.php
@@ -4,44 +4,53 @@
 $a = 50;
 $b = 10;
 
-       >    {
+ if($a > $b) {
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
-    echo "<br><br>Hello World2";
+   if($a !== $b) {
+    echo "<br>"."<br>" . "Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+if ($a== $b) {
     echo "<br><br>Oui";
 }
 
-{
+else{
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+ if($a == $b) {
     echo "1";
-}  ($a > $b) {
+} elseif ($a > $b) {
     echo "2";
 }
-{
+else{
     echo "3";
 }
 
 
 //Cinquieme ligne
+$color = "fefefe";
 echo"<br><br>";
 
- ($color) {
-    "red":
+ switch($color) {
+    case "red":
     echo "Hello";
-   "green":
+        break;
+
+   case "green":
     echo "Welcome";
+       break;
+
+     default:
+         echo "La valeur n'est nis égal a green nis égal a red";
+
 }
+
 
